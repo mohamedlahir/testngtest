ba6258286e4efb90cf94e7dea479dfdc1161f3e7
white <mohamedlahir1234@gmail.com>
2022-11-28 14:02:43 +0530

    s

--- Dinamic V3/testngbatchfile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\testngbatchfile
-cd C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\
-set classpath=C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\bin;C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\lib\*
-java org.testng.TestNG C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\testng.xml
-pause
