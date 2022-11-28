6ba5ef0d485317ebc843d085acf812b8af4b1da0
white <mohamedlahir1234@gmail.com>
2022-11-28 16:04:25 +0530

    ss

--- Dinamic V3/batchfile.bat
+++
@@ -1,8 +0,0 @@
-set projectLocation=C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\testngbatchfile
-cd C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\
-set classpath=C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\;C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\
-java org.testng.TestNG C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\testng.xml
-pause
-
-
-cd C:\Users\white\git\testNG-Dinamicv3\Dinamic V3
---
+++ Dinamic V3/jenkins.bat
@@ -0,0 +1,7 @@
+set projectLocation=C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\
+cd C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\
+set classpath=C:\Users\white\.p2*;
+java org.testng.TestNG C:\Users\white\git\testNG-Dinamicv3\Dinamic V3\testng.xml
+pause
+
+
