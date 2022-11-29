7325d3778c227dcc296efe3ae833000bfa253063
white <mohamedlahir1234@gmail.com>
2022-11-29 10:54:04 +0530

    deletecategory

---
+++ Dinamic V3/src/com/dinamic/v3/test/Registration.java
@@ -0,0 +1,48 @@
+package com.dinamic.v3.test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class Registration {
+	//
+	
+	String url = "https://posv3.dinamic.io/register";
+	String email = "taj@taj.com";
+	String password = "Lahir@123";
+
+	List<String> categoryNames = new ArrayList<>();
+	List<Integer> categoryRanks = new ArrayList<>();
+
+	@Test
+	public void registration() throws InterruptedException {
+
+		
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\white\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		
+
+		driver.manage().window().maximize();
+		driver.get(url);
+
+		driver.findElement(By.id("name")).sendKeys("Taj");
+		driver.findElement(By.id("email")).sendKeys("taj@taj.com");
+		driver.findElement(By.id("password")).sendKeys("Lahir@123");
+		Select dropDown = new Select(driver.findElement(By.id("food_type")));
+		dropDown.selectByVisibleText(" India (INR) ");
+		driver.findElement(By.id("m_pin")).sendKeys("1234");
+		driver.findElement(By.id("c_pin")).sendKeys("1234");
+		driver.findElement(By.xpath("//button[@class='btn btn-purple btn-block br-4 pd-8']"));
+	
+		driver.quit();
+	}
+}
