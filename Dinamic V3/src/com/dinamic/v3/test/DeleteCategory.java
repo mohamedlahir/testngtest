fd0c4a00ea9568183a990b8b7a3cfb4464ff62b3
white <mohamedlahir1234@gmail.com>
2022-11-29 10:28:15 +0530

    scscscsc

--- Dinamic V3/src/com/dinamic/v3/test/DeleteCategory.java
+++ Dinamic V3/src/com/dinamic/v3/test/DeleteCategory.java
@@ -13,24 +13,20 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class AddCategory {
-	//
-	
-	String url = "https://posv3.dinamic.io/";
-	String email = "admin@kfc.in";
-	String password = "Lahir@123";
+public class DeleteCategory {
+
+	String url = "https://web.dinamic.io/";
+	String email = "ops@thepumphouse.in";
+	String password = "TPH@dinamic02";
 
 	List<String> categoryNames = new ArrayList<>();
 	List<Integer> categoryRanks = new ArrayList<>();
-
+	
 	@Test
-	public void categoryAdding() throws InterruptedException {
-
+	public void deletingCategory(){
 		
-//
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\white\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://pos.dinamic.io");
@@ -53,13 +49,7 @@ public class AddCategory {
 		driver.findElement(By.id("password")).sendKeys(password);
 		driver.findElement(By.xpath("//div[@class='pos-rel']")).click();
 		driver.findElement(By.xpath("//div[@class='d-flex align-items-center push-48-t push-48-b msvg']")).click();
-		driver.findElement(By.xpath("//button[normalize-space()='1']")).click();
-		driver.findElement(By.xpath("//button[normalize-space()='2']")).click();
-		driver.findElement(By.xpath("//button[normalize-space()='3']")).click();
-		driver.findElement(By.xpath("//button[normalize-space()='4']")).click();
-
-		
-		
 		driver.quit();
 	}
+	
 }
