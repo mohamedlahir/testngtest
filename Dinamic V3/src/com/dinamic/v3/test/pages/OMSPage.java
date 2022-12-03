8de6a6fddcd34cea6647caeeca6e85ad59926bf9
white <mohamedlahir1234@gmail.com>
2022-12-03 16:11:24 +0530

    Orders

--- Dinamic V3/src/com/dinamic/v3/test/pages/OMSPage.java
+++ Dinamic V3/src/com/dinamic/v3/test/pages/OMSPage.java
@@ -1,11 +1,15 @@
 package com.dinamic.v3.test.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 
 public class OMSPage {
+	
+	int count = 0;
 
 	private WebDriver driver;
 
@@ -40,7 +44,6 @@ public class OMSPage {
 	public void order() {
 
 		driver.findElement(orders).click();
-		String pin[] = { "1", "2", "3", "4" };
 		By pin1 = By.xpath("//button[normalize-space()='1']");
 		By pin2 = By.xpath("//button[normalize-space()='2']");
 		By pin3 = By.xpath("//button[normalize-space()='3']");
@@ -59,19 +62,99 @@ public class OMSPage {
 	}
 
 	public void ordering() {
+
 		By start = By.xpath(
 				"(//button[@class='btn btn-view start-btn w-100 ng-star-inserted'][normalize-space()='start'])[1]");
 		driver.findElement(start).click();
-		By numberOfPeople = By.id("people_count");
+		System.out.println("Start Button is clicked");
+		By numberOfPeople = By.xpath("(//input[@id='people_count'])[3]");
 		driver.findElement(numberOfPeople).click();
-		driver.findElement(numberOfPeople).sendKeys("9");
-		WebElement element = driver.findElement(numberOfPeople);
-		By startButton = By.id("people_count");
+		driver.findElement(numberOfPeople).sendKeys("3");
+
+		By startButton = By.xpath("//span[normalize-space()='Start']");
+
 		driver.findElement(startButton).click();
-		System.out.println("orderinggg completed");
+		By floorName = By.xpath("//span[@class='text-primary cursor']");
+		By tableName = By.xpath("//a[@class='d-flex align-items-center ng-star-inserted']");
+		String floor = driver.findElement(floorName).getText();
+		String tableNumber[] = driver.findElement(tableName).getText().split("chevron_right");
+		System.out.println("Floor Name : " + floor);
+		System.out.println("Table Number : " + tableNumber[1]);
 	}
 
 	public void orderingItems() {
 
+//		By allCategoryItemsName = By.xpath("//p[@class=\"text-limit mb-0 item-name\"]");
+//
+//		By allCategoryItemsPrice = By.xpath("//p[@class=\"mb-0 item-price\"]");
+//		// span[@class="ng-star-inserted"]
+//
+//		By allCategoryItemsCategory = By.xpath("//span[@class='ng-star-inserted']");
+//
+//		List<WebElement> allCategoryItemsList = driver.findElements(allCategoryItemsName);
+//		List<WebElement> allCategoryItemsPriceList = driver.findElements(allCategoryItemsPrice);
+//		List<WebElement> allCategoryItemsCategoryList = driver.findElements(allCategoryItemsCategory);
+//
+//		for (int i = 0; i < allCategoryItemsList.size(); i++) {
+//			System.out.println("Item Name : " + allCategoryItemsList.get(i).getText());
+//			System.out.println("Item Price : " + allCategoryItemsPriceList.get(i).getText());
+//			System.out.println("Item Category : " + allCategoryItemsCategoryList.get(i).getText());
+//
+//		}
+//		System.out.println("Printing only first 20 items as the page has PAGINATION Concept implemented");
+//		System.out.println("Item Count : " + allCategoryItemsList.size());
+		By itemCards = By
+				.xpath("//div[@class='oms-container-category__container__content__body__card p-2 card-ripple']");
+
+		List<WebElement> allItemsCards = driver.findElements(itemCards);
+		for (int j = 0; j < 5; j++) {
+			allItemsCards.get(j).click();
+		}
+
+		By confirmaKOT = By.xpath("//span[normalize-space()='Confirm KOT']");
+		driver.findElement(confirmaKOT).click();
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		By orderedItem = By.xpath("//div[@class='d-flex align-items-center']");
+		List<WebElement> orderedItems = driver.findElements(orderedItem);
+		By orderedItemsPrice = By.xpath("//div[@class='ft-10 prtxt-right text-right doNotPrint']");
+		List<WebElement> orderItemsPriceList = driver.findElements(orderedItemsPrice);
+//		System.out.println(orderedItems);
+		
+		for (int k = 0; k < orderedItems.size(); k++) {
+			String orderd[] = orderedItems.get(k).getText().split("change_history");
+//			System.out.println("Ordered Items : " + orderedItems.get(k).getText());
+			count++;
+
+			System.out.println("Ordered Items : " + orderd[1]);
+			System.out.println("Rs : " + orderItemsPriceList.get(k).getText());
+		}
+		System.err.println("Number of items orderd : " + count);
+		By expand = By.xpath("//i[normalize-space()='expand_more']");
+		WebElement expandButton = driver.findElement(expand);
+		expandButton.click();
+		
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String itemCount;
+		By itemCountElement = By.cssSelector("div[class='mb-2 clearfix ft-11 ng-star-inserted'] div div[class='text-right'] span");
+		
+		itemCount = driver.findElement(itemCountElement).getText();
+		System.out.println("itemCount"+itemCount);
+		int itemCountParsed;
+		 itemCountParsed =Integer.parseInt(itemCount);
+		 System.out.println("itemCountParsed : "+itemCountParsed);
+		Assert.assertEquals(count, itemCountParsed);
+		
+		}
 	}
-}
+
+
--- Dinamic V3/src/com/dinamic/v3/test/tescases/OrdersTestCase.java
+++ Dinamic V3/src/com/dinamic/v3/test/tescases/OrdersTestCase.java
@@ -2,26 +2,25 @@ package com.dinamic.v3.test.tescases;
 
 import java.time.Duration;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.dinamic.v3.test.pages.OMSPage;
 
-public class LoginTestCase {
-	 String  emailAddress = "admin@kfc.in";
-	 String password = "Lahir@123";
-	 String loginPageURL = "https://posv3.dinamic.io/login";
-		
-	
-    WebDriver driver;
-  
+public class OrdersTestCase {
+	String emailAddress = "admin@kfc.in";
+	String password = "Lahir@123";
+	String loginPageURL = "https://posv3.dinamic.io/login";
+
+	WebDriver driver;
+
 	@BeforeTest
 	public void browserConfiguration() {
-		
+
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\white\\Downloads\\chromedriver_win32 (4)\\chromedriver.exe");
 		driver = new ChromeDriver();
@@ -29,35 +28,40 @@ public class LoginTestCase {
 		driver.get(loginPageURL);
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test(enabled = true, priority = 0)
-	public void log()
-	{
+	public void log() {
 		OMSPage oPage = new OMSPage(driver);
-        oPage.enterUsername(emailAddress);
-        oPage.enterPassword(password);
+		oPage.enterUsername(emailAddress);
+		oPage.enterPassword(password);
 		oPage.clicklogin();
 
 	}
-	
+
 	@Test(enabled = true, priority = 1)
-	public void ordersEntry()
-	{
+	public void ordersEntry() {
 		OMSPage oPage = new OMSPage(driver);
 		oPage.order();
-		
+
 		oPage.ordering();
-		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,350)", "1000");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		oPage.orderingItems();
 	}
 
 	@Test(enabled = false)
-	public void logout()
-	{
-		
+	public void logout() {
+
 	}
+
 	@AfterTest
-	public void close()
-	{
+	public void close() {
 		driver.quit();
 	}
 }
